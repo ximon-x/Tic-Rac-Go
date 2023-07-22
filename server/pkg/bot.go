ecf4a5ab33c06e7dd573ec107311f9bbf52626f8
Simon <salviensky@gmail.com>
2023-07-22 02:33:22 +0100

    refactor: moved functions into appropraite files

--- server/pkg/bot.go
+++ server/pkg/bot.go
@@ -1,43 +1,13 @@
 package pkg
 
 import (
-	"crypto/rand"
-	"math/big"
-
 	"github.com/gofiber/fiber/v2"
 )
 
-func makeMove(board *[][]int, player string) ([]int, error) {
-	var NoOpenSpotsError error
-	var openSpots [][]int
-
-	max := big.NewInt(100000000)
-	randInt, err := rand.Int(rand.Reader, max)
-	if err != nil {
-		return nil, NoOpenSpotsError
-	}
-
-	for i, row := range *board {
-		for j, cell := range row {
-			if cell == 0 {
-				openSpots = append(openSpots, []int{i, j})
-			}
-		}
-	}
-
-	if len(openSpots) == 0 {
-		return nil, NoOpenSpotsError
-	}
-
-	move := openSpots[randInt.Int64()%int64(len(openSpots))]
-
-	if player == "X" {
-		(*board)[move[0]][move[1]] = 2
-	} else {
-		(*board)[move[0]][move[1]] = 1
-	}
-
-	return move, nil
+type Game struct {
+	Board    [][]int `json:"board"`
+	Player   string  `json:"player"`
+	GameOver bool    `json:"game_over"`
 }
 
 func Play(c *fiber.Ctx) error {
--- server/pkg/logic.go
+++ server/pkg/logic.go
@@ -1,10 +1,9 @@
 package pkg
 
-type Game struct {
-	Board    [][]int `json:"board"`
-	Player   string  `json:"player"`
-	GameOver bool    `json:"game_over"`
-}
+import (
+	"crypto/rand"
+	"math/big"
+)
 
 func checkWin(board [][]int) bool {
 	n := len(board)
@@ -71,4 +70,37 @@ func checkWin(board [][]int) bool {
 	} else {
 		return false
 	}
+}
+
+func makeMove(board *[][]int, player string) ([]int, error) {
+	var NoOpenSpotsError error
+	var openSpots [][]int
+
+	max := big.NewInt(100000000)
+	randInt, err := rand.Int(rand.Reader, max)
+	if err != nil {
+		return nil, NoOpenSpotsError
+	}
+
+	for i, row := range *board {
+		for j, cell := range row {
+			if cell == 0 {
+				openSpots = append(openSpots, []int{i, j})
+			}
+		}
+	}
+
+	if len(openSpots) == 0 {
+		return nil, NoOpenSpotsError
+	}
+
+	move := openSpots[randInt.Int64()%int64(len(openSpots))]
+
+	if player == "X" {
+		(*board)[move[0]][move[1]] = 2
+	} else {
+		(*board)[move[0]][move[1]] = 1
+	}
+
+	return move, nil
 }
